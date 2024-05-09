f309c1744a0b34df9a6131d99beaba86ba0f0078
HmF6D43LP9xVE1y7 <info@celineloetscher.ch>
2024-05-09 20:12:45 +0200

    007

---
+++ js/carouselIndividual.js
@@ -0,0 +1,90 @@
+var carousel = new Carousel({
+    elem: 'carousel001', // target
+    dots: true, // show nav dots
+    arrows: true, // show nav arrows
+    infinite: true, // is infinite
+    initial: 0, // initial slides
+    buttons: false, // autoplay buttons
+    interval: 2000, // autplay interval in ms
+    autoplay: false, // autoplay
+})
+var carousel = new Carousel({
+    elem: 'carousel002', // target
+    dots: true, // show nav dots
+    arrows: true, // show nav arrows
+    infinite: true, // is infinite
+    initial: 0, // initial slides
+    buttons: false, // autoplay buttons
+    interval: 2000, // autplay interval in ms
+    autoplay: false, // autoplay
+})
+var carousel = new Carousel({
+    elem: 'carousel003', // target
+    dots: true, // show nav dots
+    arrows: true, // show nav arrows
+    infinite: true, // is infinite
+    initial: 0, // initial slides
+    buttons: false, // autoplay buttons
+    interval: 2000, // autplay interval in ms
+    autoplay: false, // autoplay
+})
+var carousel = new Carousel({
+    elem: 'carousel004', // target
+    dots: true, // show nav dots
+    arrows: true, // show nav arrows
+    infinite: true, // is infinite
+    initial: 0, // initial slides
+    buttons: false, // autoplay buttons
+    interval: 2000, // autplay interval in ms
+    autoplay: false, // autoplay
+})
+var carousel = new Carousel({
+    elem: 'carousel005', // target
+    dots: true, // show nav dots
+    arrows: true, // show nav arrows
+    infinite: true, // is infinite
+    initial: 0, // initial slides
+    buttons: false, // autoplay buttons
+    interval: 2000, // autplay interval in ms
+    autoplay: false, // autoplay
+})
+var carousel = new Carousel({
+    elem: 'carousel006', // target
+    dots: true, // show nav dots
+    arrows: true, // show nav arrows
+    infinite: true, // is infinite
+    initial: 0, // initial slides
+    buttons: false, // autoplay buttons
+    interval: 2000, // autplay interval in ms
+    autoplay: false, // autoplay
+})
+var carousel = new Carousel({
+    elem: 'carousel007', // target
+    dots: true, // show nav dots
+    arrows: true, // show nav arrows
+    infinite: true, // is infinite
+    initial: 0, // initial slides
+    buttons: false, // autoplay buttons
+    interval: 2000, // autplay interval in ms
+    autoplay: false, // autoplay
+})
+var carousel = new Carousel({
+    elem: 'carousel008', // target
+    dots: true, // show nav dots
+    arrows: true, // show nav arrows
+    infinite: true, // is infinite
+    initial: 0, // initial slides
+    buttons: false, // autoplay buttons
+    interval: 2000, // autplay interval in ms
+    autoplay: false, // autoplay
+})
+var carousel = new Carousel({
+    elem: 'carousel009', // target
+    dots: true, // show nav dots
+    arrows: true, // show nav arrows
+    infinite: true, // is infinite
+    initial: 0, // initial slides
+    buttons: false, // autoplay buttons
+    interval: 2000, // autplay interval in ms
+    autoplay: false, // autoplay
+})
